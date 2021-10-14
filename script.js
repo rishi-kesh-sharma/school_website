198374f5177a9b30dd3f33c714149d0e0ff2eac1
rishikesh sharma <rks83601@gmail.com>
2021-10-14 12:20:36 +0545

    updated git

--- script.js
+++ script.js
@@ -104,3 +104,83 @@ slideRightButton.addEventListener("click",(e)=>{
 })
 
 
+// script for announcement
+
+const announcementLink=document.querySelector(".announcement a");
+const announcement=document.querySelector(".announcement");
+
+const announcementContent=document.querySelector(".announcement ul");
+console.log(announcementContent);
+
+announcement.addEventListener("mouseover",(e)=>{
+announcementContent.style.display="block";
+})
+
+//   announcementContent.addEventListener("mouseout",(e)=>{
+//     announcementContent.style.display="none";
+// })
+  announcement.addEventListener("mouseout",(e)=>{
+    announcementContent.style.display="none";
+})
+
+
+//notice silder
+
+// const noticeCardContainer=document.querySelector(".notice-card-container");
+// const noticeCards=document.querySelectorAll(".notice-card");
+// var dx=0.0001;
+// var noticeCardWidth=0;
+// var noticeCardMargin=10;
+// var totalDisplaceMent=0;
+
+
+// const intervalToslideNotice=()=>{
+    
+//     var currentNoticeContainerWidth=noticeCardContainer.getBoundingClientRect().width;
+//     var currentWindowWidth=window.innerWidth;
+//     noticeCards.forEach((noticeCard)=>{
+//         console.log(noticeCard)
+//         noticeCard.style.position="absolute";
+//     var  noticeCardWidth=noticeCard.getBoundingClientRect().width;
+
+//         console.log(dx);
+//         console.log(noticeCardWidth);
+        
+        
+//         var currentXPos=noticeCard.getBoundingClientRect().x;
+//             console.log(currentXPos);
+//             console.log(currentNoticeContainerWidth);
+//             if(currentXPos+noticeCardWidth>currentNoticeContainerWidth)
+//             {
+//                 console.log()
+//                currentXPos=0;
+//             }
+//             console.log(currentXPos)
+//             else{
+//                  currentXPos=noticeCard.getBoundingClientRect().x;
+
+//                  currentXPos+=-noticeCardWidth;
+//                  }
+//         console.log(currentXPos);
+//         console.log(currentXPos);
+//         console.log(currentXPos);
+
+//         noticeCard.style.left=currentXPos+"px";
+         
+//         ((currentXPos>currentWindowWidth)&&(currentXPos<0))?noticeCard.style.display="none":noticeCard.style.display="block";
+//     })
+// }
+// noticeCards.forEach((noticeCard)=>{
+
+//     noticeCard.style.position="absolute";
+//      totalDisplaceMent+=noticeCardWidth+noticeCardMargin;
+//     noticeCard.style.left=totalDisplaceMent+"px";
+//     console.log(noticeCard.style.left);
+//     noticeCard.style.top="0px"
+//      noticeCardWidth=noticeCard.getBoundingClientRect().width;
+     
+     
+     
+//     })
+    
+//     setInterval(intervalToslideNotice,100);
